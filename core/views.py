535365794a5fdfe31512fe65434e5f48b2ec73d1
Albany <albany.prince@gmail.com>
2021-02-27 19:26:35 -0500

    still working

--- core/views.py
+++ core/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'slappersbe/home.html')
+    return render(request, 'home.html', {})
+    # Was having troule with line 5. I was receiving a error due incorrectly calling the location of the html file( via a comment from Mike on Slack. Finally got it to work.😄)
--- slappersbe/urls.py
+++ slappersbe/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.home, name='home')
 ]
 
 if settings.DEBUG:
